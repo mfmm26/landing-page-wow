78dac0b8196161f245d836a5cd2e323732576ec1
Mahmoud Faraj Muhammad Mahmoud <122284761+mfmm26@users.noreply.github.com>
2024-07-27 14:14:54 +0200

    landing

---
+++ main.js
@@ -0,0 +1,9 @@
+let mega = document.querySelector(".mega-menu");
+let other = document.querySelector(".otherLinks");
+
+
+other.addEventListener("click", function () {
+  mega.classList.toggle("active");
+});
+
+
